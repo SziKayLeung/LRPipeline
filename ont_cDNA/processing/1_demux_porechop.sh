0accad0bd0597c8cf079a32d9be3c9d51c299664
sl693 <sl693@exeter.ac.uk>
2024-12-06 12:07:05 +0000

    codeRefactor: only runs merge and porechop on samples if not already done so
    - gunzip fastq after merging
    - run porechop only if log files not present

--- ont_cDNA/processing/1_demux_porechop.sh
+++ ont_cDNA/processing/1_demux_porechop.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem=200G # specify bytes of memory to reserve
 #SBATCH --ntasks-per-node=16 # specify number of processors per node
 #SBATCH --mail-type=END # send email at job completion
-#SBATCH --array=0-19
+#SBATCH --array=16
 #SBATCH --output=1_demux_porechop-%A_%a.o
 #SBATCH --error=1_demux_porechop-%A_%a.e
 
@@ -21,7 +21,7 @@ date -u
 
 # source function script
 module load Miniconda2/4.3.21
-source activate lrp
+source activate sqanti2_py3
 source ${SCRIPT_ROOT}/processing/01_source_functions.sh
 
 # load config file provided on command line when submitting job
@@ -30,23 +30,29 @@ if [ -z "$1" ]; then
     echo "Error: No config file provided."
     exit 1
 fi
-echo "Loading config file for project: $1" 
-source $1
+config=$(realpath "$1")
+echo "Loading config file for project: ${config}"
+source ${config}
 
 ##-------------------------------------------------------------------------
 
-splitFastq=($(ls ${WKD_ROOT}/1_demultiplex/split/splitfastq*))
+splitFastq=($(ls ${WKD_ROOT}/1_demultiplex/split/splitfastq*.txt))
 SamplePath=${splitFastq[${SLURM_ARRAY_TASK_ID}]}
 Sample=$(basename ${SamplePath} .txt)
 
 echo "Merging fastq from ${SamplePath}"
-cat $(grep -v '^#' ${SamplePath}) > ${WKD_ROOT}/1_demultiplex/split/${Sample}.fastq
+if [ ! -f ${WKD_ROOT}/1_demultiplex/split/${Sample}.fastq ]; then 
+  cat $(grep -v '^#' ${SamplePath}) > ${WKD_ROOT}/1_demultiplex/split/${Sample}.fastq.gz
+  gunzip ${WKD_ROOT}/1_demultiplex/split/${Sample}.fastq.gz
+fi
 
 echo "Processing ${Sample}"
 
 # 3) run_porechop <raw.fastq.gz> <output_dir>
-splitFastqPath=${WKD_ROOT}/1_demultiplex/split/${Sample}.fastq
-run_porechop ${splitFastqPath} ${WKD_ROOT}/1_demultiplex/split/${Sample} > ${WKD_ROOT}/1b_demultiplex_merged/log/${Sample}.log
+if [ ! -f ${WKD_ROOT}/1_demultiplex/split/${Sample}.log ]; then
+  splitFastqPath=${WKD_ROOT}/1_demultiplex/split/${Sample}.fastq
+  run_porechop ${splitFastqPath} ${WKD_ROOT}/1_demultiplex/split/${Sample} > ${WKD_ROOT}/1_demultiplex/split/${Sample}.log
+fi
 
 
 ##-------------------------------------------------------------------------
