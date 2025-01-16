0e779fd6144dda40f13d189e8523dd6d6f8b3b83
sl693 <sl693@exeter.ac.uk>
2025-01-16 17:35:06 +0000

    QC: run read_QC_files.R in QC submission script

--- ont_cDNA/processing/4_QC.sh
+++ ont_cDNA/processing/4_QC.sh
@@ -2,7 +2,7 @@
 #SBATCH --export=ALL # export all environment variables to the batch job
 #SBATCH -D . # set working directory to .
 #SBATCH -p mrcq # submit to the parallel queue
-#SBATCH --time=1:00:00 # maximum walltime for the job
+#SBATCH --time=7:00:00 # maximum walltime for the job
 #SBATCH -A Research_Project-MRC148213 # research project to submit under
 #SBATCH --nodes=1 # specify number of nodes
 #SBATCH --ntasks-per-node=16 # specify number of processors per node
@@ -37,6 +37,13 @@ source activate lrp
 #ERCC_WKD_ROOT="/lustre/projects/Research_Project-MRC190311/longReadSeq/ONTRNA/sorted_nuclei/RNA/human/combined/v2"
 #studyName="Humansortednucleidataset"
 
+if [ -f ${WKD_ROOT}/QC_input.RData ]; then 
+	echo "Input for QC ready"
+else 
+	echo "Prepare input for QC"
+	Rscript ${SCRIPT_ROOT}/QC/read_QC_files.R -m ${manifest} -r ${WKD_ROOT}
+fi 
+
 if [ "${ERCC_WKD_ROOT}" == "NULL" ]; then
   ERCCDirParam="NULL"
 else
@@ -48,6 +55,5 @@ Rscript -e "rmarkdown::render('${SCRIPT_ROOT}/QC/QC_report.Rmd', output_file='${
     rootDir = '${WKD_ROOT}', 
     LRPipelineDir = '${SCRIPT_ROOT}', 
     LOGenDir = '${LOGEN_ROOT}', 
-    manifest = '${manifest}', 
     ERCCDir = ${ERCCDirParam}
   ))"
