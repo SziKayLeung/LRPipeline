ad922700ec1e521170a864d8ddde049fea79fb91
sl693 <sl693@exeter.ac.uk>
2025-09-12 17:03:57 +0100

    add pipeline script for sqanti

--- ont_cDNA/run_pipeline.sh
+++ ont_cDNA/run_pipeline.sh
@@ -9,64 +9,76 @@
 #SBATCH --mem=200G # specify bytes of memory to reserve
 #SBATCH --ntasks-per-node=16 # specify number of processors per node
 #SBATCH --mail-type=END # send email at job completion
-#SBATCH --output=run_pipeline.o
-#SBATCH --error=run_pipeline.e
-
+#SBATCH -o /dev/null  
+#SBATCH -e /dev/null   
 
 ##-------------------------------------------------------------------------
 
 echo Job started on:
 date -u
 
+# source config and function
+module load Miniconda2/4.3.21
+
 # load config file provided on command line when submitting job
 config=$(realpath "$1")
-echo "Loading config file for project: ${config}" 
-source ${config}
+if [ -z "$1" ]; then
+    echo "Error: No config file provided."
+    exit 1
+else
+    echo "Loading config file for project: ${config}" 
+    source ${config}
+    source ${SCRIPT_ROOT}/processing/01_source_functions.sh
+fi
 
+##-------------------------------------------------------------------------
+
+# create folders
+if [ ${DEMULTIPLEX:-TRUE} == "TRUE" ]; then
+  mkdir -p ${WKD_ROOT}/1_basecalled
+else
+  mkdir -p ${WKD_ROOT}/1_demultiplex ${WKD_ROOT}/1b_demultiplex_merged
+fi 
+mkdir -p ${WKD_ROOT}/0_log
+mkdir -p ${WKD_ROOT}/2_cutadapt_merge ${WKD_ROOT}/3_minimap ${WKD_ROOT}/4_tclean ${WKD_ROOT}/5_cupcake
+mkdir -p ${WKD_ROOT}/5_cupcake/5_align  
+mkdir -p $WKD_ROOT/5_cupcake/5_align/combined
+mkdir -p $WKD_ROOT/5_cupcake/6_collapse $WKD_ROOT/5_cupcake/7_sqanti3
+
+# Redirect output manually to ensure it goes there
+exec > >(tee -a "${WKD_ROOT}/0_log/run_pipeline.o") 2> >(tee -a "${WKD_ROOT}/0_log/run_pipeline.e" >&2)
+
+
+##-------------------------------------------------------------------------
+
+# only if multiplexing is performed 
 if [ "${MULTIPLEXING}" == TRUE ]; then 
-  
-  echo "Multiplexing Mode: TRUE"
 
-  if [ "${DEMULTIPLEX}" == "TRUE" ]; then 
-  
+  if [ "${DEMULTIPLEX}" == TRUE ]; then 
+
     if [ "${SEQUENCING}" == "targeted" ]; then
-      
+
       ls ${raw_merged_fastq_files}/*fastq* > ${WKD_ROOT}/1_demultiplex/split/all_fastq.txt
       split -n l/20 -d --additional-suffix=.txt ${WKD_ROOT}/1_demultiplex/split/all_fastq.txt ${WKD_ROOT}/1_demultiplex/split/splitfastq_
       echo "Performed targeted sequencing or use of custom barcodes: using Porechop for demultiplexing primers and barcodes"
       jobid1=$(sbatch ${SCRIPT_ROOT}/processing/1_demux_porechop.sh ${config} | awk '{print $NF}')
-    
+
     else
       echo "Performed whole transcriptome sequencing or use of standard barcodes: using Pychopper for demultiplexing primers and barcodes"
       jobid1=$(sbatch ${SCRIPT_ROOT}/processing/1_demux_pychopper.sh)
-    
+
     fi
-  
+
   else
-  
-    if [ "${numBatches}" != "1" ]; then
 
-      # create a symlink between each batch with $WKD_ROOT/1_demultiplex
-      for batch in ${DEMULTIPLEX_DIR[@]}; do         
-        ln -sf ${batch} "${WKD_ROOT}/1_demultiplex"      
-      done
-      echo "Demultiplexing already performed for each batch" 
+    # create a symlink between $WKD_ROOT/1_demultiplex and already demuxed folder (overwrites)
+    ln -sf ${DEMULTIPLEX_DIR}/* "${WKD_ROOT}/1_demultiplex"
+    echo "Demultiplexing already performed"
 
-    else
-    
-      # create a symlink between $WKD_ROOT/1_demultiplex and already demuxed folder (overwrites)
-      ln -sf ${DEMULTIPLEX_DIR}/* "${WKD_ROOT}/1_demultiplex"
-      echo "Demultiplexing already performed"
-      
-    fi
-  
   fi
- 
-else
-  
-  echo "Multiplexing Mode: FALSE; Processing one sample in one flow cell"
-  
-fi  
+
+fi
+
 
 if [ "${MULTIPLEXING}" == TRUE ]; then 
   jobid1=$(sbatch --array=0-$((numSamples - 1)) ${SCRIPT_ROOT}/processing/2_cutadapt_minimap2_tclean.sh ${config} | awk '{print $NF}')
@@ -76,7 +88,7 @@ else
 fi
 
 # isoseq-collapse, sqanti3
-sbatch --dependency=afterok:$jobid1 ${SCRIPT_ROOT}/processing/3_merged_collapse_sqanti3.sh
+sbatch --dependency=afterok:$jobid1 ${SCRIPT_ROOT}/processing/3_merged_collapse_sqanti3.sh ${config}
 
 # QC
-#sbatch --dependency=afterok:$jobid3 ${SCRIPT_ROOT}/processing/4_QC.sh ${config}
+#sbatch ${SCRIPT_ROOT}/processing/4_QC.sh ${config}
